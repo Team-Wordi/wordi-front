fbba33f586b50b1a7f702523e2c91cc33215707c
sueleesoossoo <isoo7510@gmail.com>
2022-02-16 23:52:53 +0900

    config eslint, prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,20 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+     // typescript 표준 규칙 모음
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    // import 관련 규칙 모음
+
+    "plugin:prettier/recommended",
+    "prettier/@typescript-eslint",
+    "prettier/react",
+     // prettier 관련 규칙 모음
+  ],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['prettier', '@typescript-eslint'],
   rules: {
     'no-shadow': 'off',
     '@typescript-eslint/no-shadow': ['error'],
